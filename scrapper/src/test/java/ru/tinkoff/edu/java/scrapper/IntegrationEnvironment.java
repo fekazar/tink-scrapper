a47586937d48dc64a865fd775eaa4f8180321518
Kazarin Fyodor <fekazar@gmail.com>
2023-04-15 14:03:53 +0300

    Better IntegrationEnvironment. Simple tests for ChatService.

--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/IntegrationEnvironment.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/IntegrationEnvironment.java
@@ -7,51 +7,70 @@ import liquibase.Liquibase;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.DirectoryResourceAccessor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import ru.tinkoff.edu.java.scrapper.repository.jdbc.JdbcScrapperRepository;
+import ru.tinkoff.edu.java.scrapper.service.jdbc.JdbcChatService;
+import ru.tinkoff.edu.java.scrapper.service.jdbc.JdbcLinkService;
 
 import javax.sql.DataSource;
 import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-@ContextConfiguration(classes = IntegrationEnvironment.Config.class)
+@Slf4j
+@TestComponent
 public abstract class IntegrationEnvironment {
     public static final PostgreSQLContainer POSTGRES_CONTAINER;
-
-    @Autowired
-    protected DataSource postgresDataSource;
 
     static {
         POSTGRES_CONTAINER = new PostgreSQLContainer("postgres:latest");
         POSTGRES_CONTAINER.withDatabaseName("scrapper");
         POSTGRES_CONTAINER.start();
-    }
 
-    @Configuration
-    public static class Config {
-        @Bean("postgresDataSource")
-        public DataSource postgresDataSource() {
-            return DataSourceBuilder
-                    .create()
-                    .url(POSTGRES_CONTAINER.getJdbcUrl())
-                    .username(POSTGRES_CONTAINER.getUsername())
-                    .password(POSTGRES_CONTAINER.getPassword())
-                    .build();
-        }
+        var postgresDataSource = DataSourceBuilder
+                .create()
+                .url(POSTGRES_CONTAINER.getJdbcUrl())
+                .username(POSTGRES_CONTAINER.getUsername())
+                .password(POSTGRES_CONTAINER.getPassword())
+                .build();
 
-        @Bean
-        public Liquibase getLiquibase(DataSource postgresDataSource) throws Exception {
-            var connection = postgresDataSource().getConnection();
+        try {
+            var connection = postgresDataSource.getConnection();
 
             liquibase.database.Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
             Liquibase liquibase = new liquibase.Liquibase("master.xml", new DirectoryResourceAccessor(Path.of("migrations")), database);
-            System.out.println(liquibase.getDatabaseChangeLog().getFilePath());
             liquibase.update(new Contexts(), new LabelExpression());
-
-            return liquibase;
+            liquibase.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    @Autowired
+    JdbcScrapperRepository repository;
+
+    @Autowired
+    JdbcChatService chatService;
+
+    @Autowired
+    JdbcLinkService linkService;
+
+    @DynamicPropertySource
+    public static void setDbProperties(DynamicPropertyRegistry registry) {
+        registry.add("secrets.db_url", POSTGRES_CONTAINER::getJdbcUrl);
+        registry.add("secrets.db_user", POSTGRES_CONTAINER::getUsername);
+        registry.add("secrets.db_pass", POSTGRES_CONTAINER::getPassword);
     }
 }
--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/SimpleTest.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/SimpleTest.java
@@ -1,7 +1,15 @@
 package ru.tinkoff.edu.java.scrapper;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
+import ru.tinkoff.edu.java.scrapper.configuration.ClientConfiguration;
+import ru.tinkoff.edu.java.scrapper.configuration.PersistenceConfig;
 
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
@@ -10,19 +18,7 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 public class SimpleTest extends IntegrationEnvironment {
-    @Test
-    void simpleTest() {
-        assertNotNull(postgresDataSource);
-    }
-
-    @Test
-    void createsTablesTest() throws SQLException {
-        var conn = postgresDataSource.getConnection();
-
-        var statement = conn.createStatement();
-        assertDoesNotThrow(() -> statement.execute("SELECT * FROM chats;"));
-        assertDoesNotThrow(() -> statement.execute("SELECT * FROM links;"));
-    }
 }
