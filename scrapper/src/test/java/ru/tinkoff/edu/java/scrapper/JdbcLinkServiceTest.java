cb46b06fb7d2973467c320a040b90227e01c9508
Kazarin Fyodor <fekazar@gmail.com>
2023-05-01 16:19:45 +0300

    Simple add link test for Jdbc.

--- scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/JdbcLinkServiceTest.java
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/JdbcLinkServiceTest.java
@@ -1,11 +1,15 @@
 package ru.tinkoff.edu.java.scrapper;
 
+import io.swagger.v3.oas.models.security.OAuthFlow;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import ru.tinkoff.edu.java.scrapper.repository.jpa.JpaLinkRepository;
+import ru.tinkoff.edu.java.scrapper.repository.pojo.Link;
+
+import java.time.OffsetDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,7 +25,14 @@ public class JdbcLinkServiceTest extends JdbcIntegrationEnvironment {
     @Test
     void addLinkTest() {
         jdbcChatService.add(1);
-        assertDoesNotThrow(() -> jdbcScrapperRepository.addLink("test_url", 1));
+
+        var link = new Link(1, "test_url_1", 1);
+        link.setLastUpdate(OffsetDateTime.now());
+
+        jdbcScrapperRepository.addTypedLink(link, "default");
+
+        var links = jdbcScrapperRepository.getAllLinks();
+        assertEquals(1, links.size());
     }
 
     @Test
